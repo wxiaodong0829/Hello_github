5557bd2a04ac3d69196c57eee061fda4e6b6a465
wxiaodong0829 <wxiaodong0829@163.com>
2012-11-13 17:50:28 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+CC = gcc
+FLAGS = -g -Wall
+
+all:
+	cc *.c -o hello_github
+
+
---
+++ hello_github.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+    > File Name: hello_github.c
+    > Author: wxiaodong
+    > Mail  : wxiaodong0829@163.com 
+    > 自由  谐作   创造   沟通
+    > Created Time: 2012年11月13日 星期二 17时45分11秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("hello github\n");
+
+	return 0;
+}
